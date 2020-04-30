11f23f5224790b422075dc18d891b99d4ab83ea5
rodcar <ivanrod12@hotmail.com>
2020-04-29 22:19:23 -0500

    Agrega 4 validaciones de los criterios de aceptación de la vista de Registro 1
    Dado que paciente se encuentra en el formulario de creación de cuenta , cuando seleccione el botón "Registrar", entonces, se verificará que todos los campos estén llenados.
    Dado que paciente se encuentra en el formulario de creación de cuenta, cuando seleccione el botón "Registrar", entonces, se verificará que el campo celular solo contenga números y que tenga 9 dígitos obligatoriamente.
    Dado que paciente se encuentra en el formulario de creación de cuenta, cuando seleccione el botón "Registrar", entonces, se verificará que el campo nombres solo contenga letras y un máximo 25 carácteres.
    Dado que paciente se encuentra en el formulario de creación de cuenta, cuando seleccione el botón "Registrar", entonces, se verificará que el campo apellidos solo contenga letras y un máximo 25 carácteres.
    
    Queda pendiente:
    
    Dado que paciente se encuentra en el formulario de creación de cuenta, cuando seleccione el botón "Registrar", entonces se verificará que la fecha de cumpleaños no sea una fecha futura, la fecha de hoy o una fecha que me de como resultado una edad menor a 2 años.

--- app/src/main/java/com/example/kututistesis/activities/Registro1Activity.java
+++ app/src/main/java/com/example/kututistesis/activities/Registro1Activity.java
@@ -17,15 +17,23 @@ public class Registro1Activity extends AppCompatActivity {
 
     private Button buttonNext;
     private EditText editTextBirthDate;
+    private EditText editTextNames;
+    private EditText editTextLastname;
+    private EditText editTextMobileNumber;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paciente_registrar);
 
-        buttonNext = (Button) findViewById(R.id.button_next);
+        // Inicializa los elementos de la vista
+        editTextNames = (EditText) findViewById(R.id.edit_text_names);
+        editTextLastname = (EditText) findViewById(R.id.edit_text_lastname);
         editTextBirthDate = (EditText) findViewById(R.id.edit_text_birthdate);
+        editTextMobileNumber = (EditText) findViewById(R.id.edit_text_mobile_number);
+        buttonNext = (Button) findViewById(R.id.button_next);
 
+        // Eventos de la vista
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -54,7 +62,16 @@ public class Registro1Activity extends AppCompatActivity {
     }
 
     private void goToRegistro2() {
-        Intent intent = new Intent(this, Registro2Activity.class);
-        startActivity(intent);
+        if (isValid()) {
+            Intent intent = new Intent(this, Registro2Activity.class);
+            startActivity(intent);
+        }
+    }
+
+    private boolean isValid() {
+        return (editTextNames.getText().toString().trim().length() > 0) &&
+                (editTextLastname.getText().toString().trim().length() > 0) &&
+                (editTextBirthDate.getText().toString().trim().length() > 0) &&
+                (editTextMobileNumber.getText().toString().trim().length() == 9);
     }
 }
