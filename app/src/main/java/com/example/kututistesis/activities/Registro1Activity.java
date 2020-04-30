b94e0cb5e9dd3fd176146f15ac44432d3674098e
rodcar <ivanrod12@hotmail.com>
2020-04-29 21:42:10 -0500

    Agrega DataPicker para la fecha de nacimiento en el registro
    Bug por resolver: Al hacer doble tap al EditText de la fecha de nacimiento se abren dos dialogos para ingresar la fecha de nacimiento

--- app/src/main/java/com/example/kututistesis/activities/Registro1Activity.java
+++ app/src/main/java/com/example/kututistesis/activities/Registro1Activity.java
@@ -2,16 +2,21 @@ package com.example.kututistesis.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
 
 import com.example.kututistesis.R;
+import com.example.kututistesis.dialog.DatePickerFragment;
 
 public class Registro1Activity extends AppCompatActivity {
 
     private Button buttonNext;
+    private EditText editTextBirthDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,6 +24,7 @@ public class Registro1Activity extends AppCompatActivity {
         setContentView(R.layout.paciente_registrar);
 
         buttonNext = (Button) findViewById(R.id.button_next);
+        editTextBirthDate = (EditText) findViewById(R.id.edit_text_birthdate);
 
         buttonNext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -26,6 +32,25 @@ public class Registro1Activity extends AppCompatActivity {
                 goToRegistro2();
             }
         });
+        editTextBirthDate.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                openDialogDatePicker();
+            }
+        });
+    }
+
+    private void openDialogDatePicker() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 because January is zero
+                final String selectedDate = day + " / " + (month+1) + " / " + year;
+                editTextBirthDate.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(Registro1Activity.this.getSupportFragmentManager(), "datePicker");
     }
 
     private void goToRegistro2() {
