025556290586d8c738db99c4d255c58cb28373a2
rodcar <ivanrod12@hotmail.com>
2020-04-29 21:08:28 -0500

    Agrega la vista de Registro 3

--- app/src/main/java/com/example/kututistesis/Registro2Activity.java
+++ app/src/main/java/com/example/kututistesis/Registro2Activity.java
@@ -2,13 +2,32 @@ package com.example.kututistesis;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Registro2Activity extends AppCompatActivity {
+
+    private Button buttonNext;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.paciente_registrar_2);
+
+        buttonNext = (Button) findViewById(R.id.button_next);
+
+        buttonNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToRegistro3();
+            }
+        });
+    }
+
+    private void goToRegistro3() {
+        Intent intent = new Intent(this, Registro3Activity.class);
+        startActivity(intent);
     }
 }
