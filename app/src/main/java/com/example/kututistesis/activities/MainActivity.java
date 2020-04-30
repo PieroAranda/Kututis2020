837138bcf14f10665501116fa2165862bf2b61ea
rodcar <ivanrod12@hotmail.com>
2020-04-30 00:25:29 -0500

    Agrega flujo de interacción con el teclado en la vista de Inicio de sesión

--- app/src/main/java/com/example/kututistesis/activities/MainActivity.java
+++ app/src/main/java/com/example/kututistesis/activities/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.graphics.Paint;
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.View;
+import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -47,6 +49,17 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 goToRegistro();
+            }
+        });
+
+        editTextPassword.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+                if (actionId == EditorInfo.IME_ACTION_DONE) {
+                    goToPaginaPrincipal();
+                    return true;
+                }
+                return false;
             }
         });
     }
