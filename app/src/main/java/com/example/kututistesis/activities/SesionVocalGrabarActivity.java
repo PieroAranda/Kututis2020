d817860f5d5d81fe5f8f1cdcc5ac9d079152c50b
rodcar <ivanrod12@hotmail.com>
2020-05-05 13:08:43 -0500

    Corrige configuración de navegación entre la vista de Fonemas vocálicos y su historial

--- app/src/main/java/com/example/kututistesis/activities/SesionVocalGrabarActivity.java
+++ app/src/main/java/com/example/kututistesis/activities/SesionVocalGrabarActivity.java
@@ -196,14 +196,11 @@ public class SesionVocalGrabarActivity extends AppCompatActivity {
 
 
     private void goToPaginaPrincipal() {
-        Intent intent = new Intent(this, PaginaPrincipalActivity.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
-        startActivity(intent);
+        onBackPressed();
     }
 
     public void goToHistorialAudios(View view) {
         Intent intent = new Intent(this, HistorialAudios.class);
-        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 
