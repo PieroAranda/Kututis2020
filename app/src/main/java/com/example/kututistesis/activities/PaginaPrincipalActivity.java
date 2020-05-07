72101ff8d5c163dd1110a1cdfc58b23f9dceed63
rodcar <ivanrod12@hotmail.com>
2020-05-06 19:33:59 -0500

    Agrega botón de cerrar sesión en la vista de Pagina Principal

--- app/src/main/java/com/example/kututistesis/activities/PaginaPrincipalActivity.java
+++ app/src/main/java/com/example/kututistesis/activities/PaginaPrincipalActivity.java
@@ -3,9 +3,11 @@ package com.example.kututistesis.activities;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import com.example.kututistesis.R;
 
@@ -14,6 +16,7 @@ public class PaginaPrincipalActivity extends AppCompatActivity {
     private Button buttonPraxias;
     private Button buttonConsonatesVocalicos;
     private Button buttonConsonatesConsonanticos;
+    private ImageView imageViewLogout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +28,7 @@ public class PaginaPrincipalActivity extends AppCompatActivity {
         buttonPraxias = (Button) findViewById(R.id.button_praxias);
         buttonConsonatesVocalicos = (Button) findViewById(R.id.button_fonemas_vocalicos);
         buttonConsonatesConsonanticos = (Button) findViewById(R.id.button_fonemas_consonanticos);
+        imageViewLogout = (ImageView) findViewById(R.id.image_logout);
 
         // Eventos de la vista
         buttonPraxias.setOnClickListener(new View.OnClickListener() {
@@ -47,6 +51,19 @@ public class PaginaPrincipalActivity extends AppCompatActivity {
                 goToConsonatesConsonanticos();
             }
         });
+
+        imageViewLogout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                logout();
+            }
+        });
+    }
+
+    private void logout() {
+        Intent intent = new Intent(this, MainActivity.class);
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
     }
 
     private void goToConsonatesConsonanticos() {
